2504783b45b6f3c5ba75564cd3dd10752111aa0c
Civoremo <omerovic_n@hotmail.com>
2019-04-05 13:14:31 -0400

    names.py runtime under 0.01 sec; answers complete

--- Data_Structures_Answers.md
+++ Data_Structures_Answers.md
@@ -1,18 +1,34 @@
 Add your answers to the questions below.
 
 1. What is the runtime complexity of your ring buffer's `append` method?
+O(1) -> We are setting value at a given index; no searching done
 
 2. What is the space complexity of your ring buffer's `append` function?
+O(1) -> We have a predefined space that does not change.
 
 3. What is the runtime complexity of your ring buffer's `get` method?
+O(1) -> Predefined size
 
 4. What is the space complexity of your ring buffer's `get` method?
+O(1) -> Predefined size
 
 
 5. What is the runtime complexity of the provided code in `names.py`?
+O(n^2) -> Nested for loops; number items could grow indefinitely 
 
 6. What is the space complexity of the provided code in `names.py`?
+O(n) -> Space could grow indefinitely
 
 7. What is the runtime complexity of your optimized code in `names.py`?
+Using BST
+O(n log n) -> Needs to loop through all names[O(n)] and BST loop [O(log n)] of that name
+
+Using set
+O(n) -> Looping through duplicate names and appending to duplicates.list
 
 8. What is the space complexity of your optimized code in `names.py`?
+Using BST
+O(n) -> Indefinite number of duplicates
+
+Using set
+O(1) -> size is chosen at initialization
--- names/names.py
+++ names/names.py
@@ -11,22 +11,31 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-bst1 = BinarySearchTree(names_1[len(names_1)-1])
-bst2 = BinarySearchTree(names_2[len(names_2)-1])
+# --- part of BST
+# bst1 = BinarySearchTree(names_1[len(names_1)-1])
+# bst2 = BinarySearchTree(names_2[len(names_2)-1])
+# for name in names_1:
+#     bst1.insert(name)
+# for name in names_2:
+#     bst2.insert(name)
 
-for name in names_1:
-    bst1.insert(name)
+# duplicates = []
+# --- using BST --- results in under 1 sec
+# for name_1 in names_1:
+#     if bst1.contains(name_1) == bst2.contains(name_1):
+#         duplicates.append(name_1)
 
-for name in names_2:
-    bst2.insert(name)
+# --- original ---
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
-duplicates = []
-for name_1 in names_1:
-    if bst1.contains(name_1) == bst2.contains(name_1):
-        duplicates.append(name_1)
-    # for name_2 in names_2:
-    #     if name_1 == name_2:
-    #         duplicates.append(name_1)
+# --- results in under 1 hundreth of sec
+names = set(names_1) & set(names_2)
+duplicates = []*len(names)
+for name in names:
+    duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
