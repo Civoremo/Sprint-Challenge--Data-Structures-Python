10fa6851c993ba772905a1e453cee7ca3904c6ab
Civoremo <omerovic_n@hotmail.com>
2019-04-05 11:18:37 -0400

    ring_buffer tests pass

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,23 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    # pass
+    # start with storage[current], change to new item
+    # if current is greater than capacity
+    # remove first item and replace with new item
+    if self.current < self.capacity:
+      self.storage[self.current] = item
+      self.current += 1
+    else:
+      self.current = 0
+      self.append(item)
+
+    
 
   def get(self):
-    pass
+    # pass
+    our_list = []
+    for item in self.storage:
+      if item is not None:
+        our_list.append(item)
+    return our_list
